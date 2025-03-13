22b671c32cef2aa623333c78476da38fdaa2d7e6
Yuting Chen <yc659@ardebil.nmr.mgh.harvard.edu>
2025-03-13 11:53:45 -0400

    Add new file: path/to/new-file

---
+++ setup.m
@@ -0,0 +1,5 @@
+function setup()
+    addpath(genpath('/autofs/space/raijin_001/users/yuting/MIMOSA/Recon/3T/utils'));
+    % Display a message indicating successful setup
+    disp('Setup complete');
+end
